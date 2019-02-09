f197f08b86e0d0f87a4cb31879e9d69ae54aa7a5
RandSaleh <rand_nn@hotmail.com>
2019-02-09 23:07:00 +0200

    add the basic functionality

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -1,19 +1,61 @@
 package com.example.gridlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer;
+    Button btn ;
+    String temTag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void sayPhrases(View view) {
+         btn = (Button) findViewById(view.getId());
+         temTag = (String) btn.getTag();
+        switch (temTag) {
+            case ("1"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.hello);
+                mediaPlayer.start();
+                break;
+            case ("2"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
+                mediaPlayer.start();
+                break;
 
+            case ("3"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
+                mediaPlayer.start();
+                break;
 
+            case ("4"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
+                mediaPlayer.start();
+                break;
+            case ("5"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
+                mediaPlayer.start();
+                break;
+            case ("6"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.please);
+                mediaPlayer.start();
+                break;
+            case ("7"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
+                mediaPlayer.start();
+                break;
+            case ("8"):
+                mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
+                mediaPlayer.start();
+                break;
 
-
+        }
 
     }
 }
